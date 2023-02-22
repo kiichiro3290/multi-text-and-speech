1e3fc70f4ff02886eab44b0837ba4b7102ad4033
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2023-02-22 23:08:21 +0900

    install mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { defaultTheme } from '~/styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = defaultTheme
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material/styles'
+
+export const defaultTheme = createTheme({
+  palette: {
+    background: {
+      default: '#F7FAFF',
+    },
+    primary: {
+      contrastText: '#FFFFFF',
+      dark: '#2329A7',
+      light: '#3581FF',
+      main: '#3C44FF',
+    },
+    secondary: {
+      contrastText: '#FFFFFF',
+      dark: '#B32F2B',
+      light: '#FF7570',
+      main: '#FF5C57',
+    },
+    text: {
+      primary: '#555555',
+      secondary: '#696969',
+    },
+  },
+})
